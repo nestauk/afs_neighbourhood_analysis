34454d89e88e934e9797127b9b328c7b8b892b9a
Rachel <rachel.wilcock@nesta.org.uk>
2022-07-11 10:45:37 +0100

    this commit will ignore githooks

--- outputs/figures/make_draft.sh
+++ outputs/figures/make_draft.sh
@@ -1,1 +1,1 @@
-pandoc --filter=pandoc-crossref -s section_exec_summary.md section_introduction.md section_data_methodology.md section_cluster_findings.md section_conclusions.md -o 2022july7_mapping_ey_v09.docx --number-sections
+pandoc --filter=pandoc-crossref -s section_exec_summary.md section_introduction.md section_data_methodology.md section_cluster_findings.md section_conclusions.md -o 2022july8_mapping_ey_v10.docx --number-sections
